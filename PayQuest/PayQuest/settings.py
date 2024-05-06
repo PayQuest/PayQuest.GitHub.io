6b74f827f9c1806be098efd7ac2aec58d70456ca
ElijahDiLullo <142916609+ElijahDiLullo@users.noreply.github.com>
2024-05-06 11:20:03 -0400

    working a bit on profiles

--- PayQuest/PayQuest/settings.py
+++ PayQuest/PayQuest/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
+MEDIA_URL = '/media/' # Public URL at the browser
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
